e87ab8fae88c36cab40a43b40bbdba3fc9a00287
hxulin <1455936807@qq.com>
2021-02-07 21:19:20 +0800

    增加 MD5Utils

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 // 版本
 export const version: string = '0.0.8'
 
+// AES 加解密
+export * from './AESUtils'
+
 // 文件处理相关
 export * from './FileUtils'
+
+// MD5 摘要计算
+export * from './MD5Utils'
 
 // 路径处理相关
 export * from './PathUtils'
 
-// 字符串处理相关
-export * from './StringUtils'
-
-// AES 加解密
-export * from './AESUtils'
-
 // 随机数、随机字符串相关
 export * from './RandomUtils'
 
+// 字符串处理相关
+export * from './StringUtils'
